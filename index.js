afd93f10c4a7cff7857f7632e2812374c230f4e9
karagarvey <kgarvey87@gmail.com>
2018-03-21 15:00:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,11 @@ function johnLennonFacts (array) {
   }
   return array
 }
+
+function iLoveTheBeatles(n){
+  var array = []
+  do {
+    array.push("I love the Beatles!")
+    n++
+  } while (n < 15)
+}
