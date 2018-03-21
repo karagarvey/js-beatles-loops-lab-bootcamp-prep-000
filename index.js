e4b4724f9a0e30cfc99bb0da864ddb6e826a2b2d
karagarvey <kgarvey87@gmail.com>
2018-03-21 14:47:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay (musicians, instruments){
   var sentence = []
-  for (var i = 0; i > sentence.length; i++){
+  for (var i = 0; i < sentence.length; i++){
     sentence.push(sentence[i] + `plays $instruments[1]`)
   }
   return sentence
