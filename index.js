de954c871bb0f361fb62f026e9c2f115e50d3d2d
karagarvey <kgarvey87@gmail.com>
2018-03-21 14:45:20 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,7 @@
+function theBeatlesPlay (musicians, instruments){
+  sentence = []
+  for (var i = 0; i > sentence.length; i++){
+    sentence.push(sentence[i] + `plays $instruments[1]`)
+  }
+  return sentence
+}
